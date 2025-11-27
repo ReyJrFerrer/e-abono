4ab262aef459d8df553b6306d5c6c8a119b512c6
Reynaldo Jr. Ferrer <157354696+ReyJrFerrer@users.noreply.github.com>
2025-11-28 01:22:51 +0800

    Updated ContactUs.tsx

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -51,7 +51,7 @@ export const ContactUs = () => {
 
     map.current.on('load', () => {
       new maplibregl.Marker({ color: '#7c9a3d' })
-        .setLngLat([120.59032, 16.44188])
+        .setLngLat([120.5902746, 16.4543609])
         .setPopup(
           new maplibregl.Popup().setHTML(
             '<div style="padding: 8px;"><strong>Benguet State University</strong><br/>La Trinidad, Benguet</div>'
